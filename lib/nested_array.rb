9648cfce6f042b6d2d499652197d5aa4d6255569
Brodrick George <brodrick.george@yahoo.com>
2020-10-16 05:12:41 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -43,11 +43,7 @@ end
 def matrix_lookup(matrix, row, column)
  
 
-produce_storage_room = [ 
-  
-   ["Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant"], 
-   ["Strawberries", "Avocadoes", "Grapes", "Potatoes", "Asparagus"]
-]
+
 
 produce_storage_room[1][3]
 

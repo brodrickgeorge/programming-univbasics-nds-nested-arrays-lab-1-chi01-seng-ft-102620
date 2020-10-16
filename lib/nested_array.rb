122e9e86c95309681671efd943dee645c29d0cc6
Brodrick George <brodrick.george@yahoo.com>
2020-10-16 03:57:32 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -44,7 +44,7 @@ def matrix_lookup(matrix, row, column)
   array = [
     ["Eggplant", "Grapefruit", "Oranges", "Pineapple", "Watermelon"], ["Asparagus", "Avocadoes", "Grapes", "Potatoes", "Strawberries"]
     ]
-  
+  array[3]
   
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column

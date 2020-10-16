a56454d9de89f9cf5056fc1823307c48d86f922d
Brodrick George <brodrick.george@yahoo.com>
2020-10-16 04:28:40 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -60,7 +60,7 @@ def matrix_update(matrix, row, column, new_value)
    ["Grapefruit", "Pineapple", "Onions", "Watermelon", "Eggplant"], 
    ["Strawberries", "Avocadoes", "Grapes", "Potatoes", "Asparagus"]
 ]
- produce_storage_room[2]
+ produce_storage_room[]
   # Given any matrix (array of arrays), a row index and a column index, 
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix

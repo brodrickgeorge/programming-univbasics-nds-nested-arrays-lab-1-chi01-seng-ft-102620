ea45f8ae18148e3a4f5b78bc90ec471499de9c0f
Brodrick George <brodrick.george@yahoo.com>
2020-10-16 05:11:35 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -49,7 +49,7 @@ produce_storage_room = [
    ["Strawberries", "Avocadoes", "Grapes", "Potatoes", "Asparagus"]
 ]
 
-
+produce_storage_room[1][1]
 
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
